622b99277cb9f311640cc32d746a9eb703576a57
Josh Cole <saikojosh@gmail.com>
2015-09-19 17:48:15 +0100

    First commit

---
+++ parseBool.js
@@ -0,0 +1,28 @@
+/*
+ * PARSE BOOL
+ * Converts a string representation of a bool into a Boolean value.
+ */
+
+module.exports = function (input) {
+
+  // Ensure we can use one switch for booleans and strings.
+  input = (typeof input === 'string' ? input.toLowerCase() : input);
+
+  // Do we have a valid representation of a bool?
+  switch (input) {
+
+    case true:
+    case 'true':  return true;
+
+    case false:
+    case 'false': return false;
+
+    case null:
+    case 'null'   return null;
+
+  }
+
+  // Last ditch attempt.
+  return Boolean(input);
+
+};
